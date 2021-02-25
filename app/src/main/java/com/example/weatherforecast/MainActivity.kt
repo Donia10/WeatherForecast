2ff13a7ecb4ff7f3c18d59149e39f35915c6bc3f
donia <donia.ashraf100@gmail.com>
2021-02-25 16:15:02 +0200

    first version of  design tabLayout

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.weatherforecast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
+import androidx.appcompat.widget.Toolbar
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolBar =findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolBar)
+        val tabLayout=findViewById<TabLayout>(R.id.tab_layout)
+
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.setting_fragment))
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.home_fragment))
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.alert_fragment))
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.favourite_fragment))
+        tabLayout.setTabMode(TabLayout.MODE_FIXED)
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL)
+        val viewPager =findViewById<ViewPager>(R.id.pager)
+         val adapter=PagerAdapter(supportFragmentManager,tabLayout.tabCount)
+        viewPager.setAdapter(adapter)
+
+        //listener
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewPager.currentItem = tab.position
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
+
     }
 }
